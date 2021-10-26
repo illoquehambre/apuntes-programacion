d1ee261d7e8bb568dd31195f06afd79472c6000f
Ignacio Moreno Gómez <Ignacio.moreno.gomez2003@gmail.com>
2021-10-26 10:42:26 +0200

    levemente modificao

--- Hola Mundo/src/holamundo/Principal.java
+++ Hola Mundo/src/holamundo/Principal.java
@@ -1,11 +1,14 @@
 package holamundo;
 
+import lectura.Leer;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int limite=26;
-	
+		System.out.println("Cuantas veces desea decir hola mundo?");
+		limite=Leer.datoInt();
 		for(int i=0;i<limite;i++) {
 			System.out.println(i+1+"º Hola muy wenos dias Mundo");
 		}
