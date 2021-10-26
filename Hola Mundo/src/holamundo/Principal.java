4e65b92d6ec6e4675afdf4271aafb647af377f5a
Ignacio Moreno Gómez <Ignacio.moreno.gomez2003@gmail.com>
2021-10-26 09:37:56 +0200

    Eclipse Workspace

---
+++ Hola Mundo/src/holamundo/Principal.java
@@ -0,0 +1,15 @@
+package holamundo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int limite=26;
+	
+		for(int i=0;i<limite;i++) {
+			System.out.println(i+1+"º Hola Mundo");
+		}
+
+	}
+
+}
